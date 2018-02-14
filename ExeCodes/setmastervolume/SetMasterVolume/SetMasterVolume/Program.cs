5b1a33f0e65ead4b1aa7638f92685f3366ef0cb1
mert4x4 <mert4x4@gmail.com>
2018-02-14 22:53:50 +0300

    the exe files which controlls sound

---
+++ ExeCodes/setmastervolume/SetMasterVolume/SetMasterVolume/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetMasterVolume
+{
+    class Program
+    {
+        static SystemVolumeConfigurator sound = new SystemVolumeConfigurator();
+        static void Main(string[] args)
+        {
+                sound.SetVolume(Convert.ToInt16(args[0]));
+        }
+    }
+}
