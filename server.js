8bef26a4170765dc5756f8c00623b28fc901ed21
mert4x4 <mert4x4@gmail.com>
2018-02-16 20:34:26 +0300

    finished

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ var io = require('socket.io')(serv,{});
 const { spawn } = require('child_process');
 
 serv.listen(2000);
+console.log("server started");
 
  io.sockets.on('connection', function(socket){
-	socket.on('IncomingData',function(data){
+	socket.on('Channel_VolumeLevel',function(data){
 		console.log(data);
 		spawn('./ExeFiles/SetMasterVolume.exe', [data.VolumeLevel]);
+	});
+	socket.on('Channel_OtherInfo',function(data){
+		console.log(data);
+		spawn('./ExeFiles/RemotePcControl.exe', [data.mode]);
 	});
 });
 /*
--- web_client/script.js
+++ web_client/script.js
@@ -4,8 +4,18 @@ var socket = io();
 var Slider_VolumeLevel = document.getElementById("VolumeLevel");
 var Button_VolumeSend = document.getElementById("sendVolumeValue");
 var Label_VoiceLevel = document.getElementById("label_voiceLevel");
+var Button_mute = document.getElementById("button_mute");
+var Button_next = document.getElementById("button_nextTrack");
+var Button_previous = document.getElementById("button_previousTrack");
+var Button_startStop = document.getElementById("button_pausePlay");
+label_voiceLevel.style.color = "yellow";
 
-Button_VolumeSend.onclick = function(){socket.emit('IncomingData',{VolumeLevel:Slider_VolumeLevel.value});Label_VoiceLevel.style.color = "green";};
+Button_VolumeSend.onclick = function(){Label_VoiceLevel.innerHTML = Slider_VolumeLevel.value; socket.emit('Channel_VolumeLevel',{VolumeLevel:Slider_VolumeLevel.value});Label_VoiceLevel.style.color = "green";};
+Button_mute.onclick = function(){socket.emit('Channel_OtherInfo',{mode:0});};
+Button_next.onclick = function(){socket.emit('Channel_OtherInfo',{mode:3});};
+Button_previous.onclick = function(){socket.emit('Channel_OtherInfo',{mode:5});};
+Button_startStop.onclick = function(){socket.emit('Channel_OtherInfo',{mode:4});};
+
 Slider_VolumeLevel.oninput = function(){
 	Label_VoiceLevel.innerHTML = this.value;
 	Label_VoiceLevel.style.color = "red";
